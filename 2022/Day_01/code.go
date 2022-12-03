05700b897c5d8ee98bca80360103c83f02db8b19
Rida Amirini <ridaamirini@rs-apps.de>
2022-12-03 03:07:44 +0100

    Day 1

---
+++ 2022/Day_01/code.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"advent-of-code/helper"
+	"fmt"
+	"path"
+	"runtime"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func main() {
+	_, f, _, _ := runtime.Caller(0)
+	cwd := path.Join(path.Dir(f))
+
+	input := helper.ReadInput(cwd, helper.NewLine)
+	iValues := input.Strings()
+
+	// Execute
+	start := time.Now()
+	result01 := part01(iValues)
+	result02 := part02(iValues)
+	executionTime := helper.ExecutionTime(time.Since(start))
+
+	fmt.Printf("Solution Part 1: %v\n", result01)
+	fmt.Printf("Solution Part 2: %v\n", result02)
+	fmt.Printf("Execution time: %s\n", executionTime)
+
+	helper.SaveBenchmarkTime(executionTime, cwd)
+
+	// Testing
+	helper.TestResults(
+		[]helper.TestingValue{
+			helper.TestingValue{Result: result01, Expect: 67027},
+			helper.TestingValue{Result: result02, Expect: 197291},
+		},
+	)
+}
+
+type CalorieList []int
+
+func NewCalorieList(input []string) CalorieList {
+	list := CalorieList{}
+
+	tmp := 0
+	for _, s := range input {
+		if "" == s {
+			list = append(list, tmp)
+			tmp = 0
+
+			continue
+		}
+
+		i, _ := strconv.Atoi(s)
+		tmp += i
+	}
+
+	return list
+}
+
+type Strategy struct {
+	rounds Rounds
+}
+
+type Rounds struct {
+	own string
+}
+
+type Choice int
+
+// Task code
+func part01(input []string) int {
+	l := NewCalorieList(input)
+	sort.Ints(l)
+
+	return l[len(l)-1]
+}
+
+func part02(input []string) int {
+	l := NewCalorieList(input)
+	sort.Ints(l)
+	length := len(l)
+
+	return l[length-1] + l[length-2] + l[length-3]
+}
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Advent of Code
 
 [2015](/2015/README.md) | [2016](/2016/README.md) | [2017](/2017/README.md) | 
-[2018](/2018/README.md) | [2019](/2020/README.md) | [2020](/2020/README.md) | [2021](/2021/README.md)
+[2018](/2018/README.md) | [2019](/2020/README.md) | [2020](/2020/README.md) | 
+[2021](/2021/README.md) | [2022](/2022/README.md)
 
 This repository contains my solutions for [Advent of Code](https://adventofcode.com/).
 
